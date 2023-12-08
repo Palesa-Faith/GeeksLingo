7c540f4b84d693f9d6e182ade1a74241a7d1eb05
Palesa <Katanepalesa@gmail.com>
2023-12-08 14:41:52 +0200

    Chat  app structure

---
+++ app/src/main/java/com/example/geekslingo/Login.kt
@@ -0,0 +1,29 @@
+package com.example.geekslingo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+          val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+}
